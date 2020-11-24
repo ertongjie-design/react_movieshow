602a2ea6751b14cd538795e2f7bcfe34fa9e9f4f
ertongjie-design <1625742910@qq.com>
2020-11-24 21:10:57 +0800

    header和sider添加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import React from 'react';
-import { HashRouter, Route, Link, BrowserRouter, Switch } from 'react-router-dom';
+import { HashRouter, Route, Link, Switch } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import HomeContainer from './components/HomeContainer';
 import MovieContainer from './components/MovieContainer';
 import AboutContainer from './components/AboutContainer';
-import ColumnGroup from 'antd/lib/table/ColumnGroup';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 class App extends React.Component{
   constructor(props){
@@ -45,15 +44,29 @@ class App extends React.Component{
             </Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '10px 50px' }}>
-          <HashRouter>
-            <Switch>
-              <Route exact path='/home' component={HomeContainer}></Route>
-              <Route exact path='/movie' component={MovieContainer}></Route>
-              <Route exact path='/about' component={AboutContainer}></Route>
-            </Switch>
-          </HashRouter>
-        </Content>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['正在热映']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <Menu.Item key="正在热映">正在热映</Menu.Item>
+              <Menu.Item key="即将上映">即将上映</Menu.Item>
+              <Menu.Item key="Top250">Top250</Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '10px 50px' }}>
+            <HashRouter>
+              <Switch>
+                <Route exact path='/home' component={HomeContainer}></Route>
+                <Route exact path='/movie' component={MovieContainer}></Route>
+                <Route exact path='/about' component={AboutContainer}></Route>
+              </Switch>
+            </HashRouter>
+          </Content>
+        </Layout>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     )
